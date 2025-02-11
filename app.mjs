1e3bebb2ec041cc6f7a63a07213573d79f43c7df
Joseph Lahai Kanu <148826159+joemrnice@users.noreply.github.com>
2025-02-11 01:27:56 +0000

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -4,6 +4,22 @@ import morgan from "morgan";
 import userAccessGranted from "./authentication.mjs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import multer from "multer";
+
+// Multer configuration for file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    },
+});
+
+// Initialize Multer with the storage configuration
+
+const upload = multer({ storage });
+
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -42,24 +58,25 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
     userAccessGranted ? res.sendFile(_dirname + "/public/pages/index.html")
-    : res.render('register.ejs');
+        : res.render('register.ejs');
 });
 
-app.post('/blogger', (req, res) => {
-    let photos = req.body["src"];
-    const photo = `<img src="images/${photos}" alt="blogger">`;
+app.post('/blogger', upload.single('blogger_image'), (req, res) => {
+    let blogger_image = req.file ? req.file.filename : null;
     let blogger = req.body["name"];
     let title = req.body["title"];
     let content = req.body["blog"];
+    let dateCreated = new Date().getFullYear();
     res.render("blog.ejs", {
-        image: photo,
+        image: blogger_image,
         blogs: blogger,
         blogTitle: title,
-        blogPost:content
+        blogPost: content,
+        created_at: dateCreated
     });
 });
 
 app.listen(port, (err) => {
     if (err) throw err;
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
