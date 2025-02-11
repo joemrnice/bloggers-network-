269d11a9a39eecc3d6db78ba29c5601449f3959b
Joseph Lahai Kanu <148826159+joemrnice@users.noreply.github.com>
2025-02-11 01:50:42 +0000

    major upgrades

--- authentication.mjs
+++ authentication.mjs
@@ -5,6 +5,7 @@ let userAccess = false;
 
 let userAccessGranted = (req, res, next) => {
     const password = req.body["password"];
+    //modify password in the condition statement to authenticate yourself
     if (password === "joemrnice$") {
         userAccess = true;
     }
