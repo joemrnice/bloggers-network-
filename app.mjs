341ef91392b7332ed42940e795145df394d492b3
Joseph Lahai Kanu <148826159+joemrnice@users.noreply.github.com>
2026-05-30 07:46:37 +0000

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -9,7 +9,7 @@ import multer from "multer";
 // Multer configuration for file uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'public/uploads');
+        cb(null, 'public/upload');
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname);
